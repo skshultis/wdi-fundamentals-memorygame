febf243571cc59cb4a0d0916a03fa76423f21b10
Sarah K Shultis <skshultis@Sks-MacBook-Air.local>
2016-06-27 06:05:08 -0400

    updated css and updated js to main2 file

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,26 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("cards");
+var cardOne; cardOne="queen";
+var cardTwo; cardTwo="queen";
+var cardThree; cardThree="king";
+var cardFour; cardFour="king";
+
+if(cardTwo===cardFour){
+	alert("Sorry, try again.");
+}	else if(cardTwo!==cardFour);{
+	alert("Sorry, try again.");
+}
+if(cardOne===cardTwo){
+	alert("You found a match!");
+}	else if(cardOne!==cardTwo);{
+	alert("Sorry, try again.");
+}
+if(cardThree===cardFour){
+	alert("You found a match!");
+}	else if(cardThree!==cardFour);{
+	alert("Sorry, try again.");
+}
+if(cardTwo===cardThree){
+	alert("Sorry, try again.");
+}	else if(cardTwo!==cardThree);{
+	alert("Sorry, try again.");
+}
---
+++ starter-code/main2.js
@@ -0,0 +1,41 @@
+console.log("cards");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var board = document.getElementById("game-board");
+
+function createBoard(){
+	for (var i=0;
+		i< cards.length;
+		i++) {
+var cardElement=document.createElement("div");
+
+cardElement.className = "card";
+
+cardElement.setAttribute("data-card", cards[i]); 
+
+cardElement.addEventListener("click", isTwoCards);
+
+board.appendChild(cardElement);
+	}
+}
+function isTwoCards()	{
+	cardsInPlay.push(this.getAttribute("data-card"));
+		if (this.getAttribute("data-card") === "queen") {
+			this.innerHTML = "<img src='http://i.imgur.com/hLACgY7.png'>";
+		}	else {
+			this.innerHTML = "<img src='http://i.imgur.com/Stl0BZa.png'>";
+		}
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+		else {
+		alert("Sorry, try again.");
+		}
+	}
+createBoard();
